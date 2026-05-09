b930a722a9c35bf40c056508df1f1cac4d30a23e
Emmanuel Mbarga <emmanuelmbarg@gmail.com>
2026-05-09 03:52:07 +0100

    Ajout d'un utilitaire pour la gestion des vagues apres le hover sur le logo

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ type Props = {}
 const Header = (props: Props) => {
   return (
     <header className='flex justify-center items-center py-4 -mb-28 '>
-      <FizziLogo/>
+      <FizziLogo className='h-20 z-10 cursor-pointer text-sky-800'/>
     </header>
   )
 }
